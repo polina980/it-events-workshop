b9f01f72cbc65708b51a198675732b9b68f1f94f
Alex Martynov <fenixdeveloper@gmail.com>
2023-09-17 11:46:47 +0600

    feat: partial refactoring for tags

--- src/UI-kit/TagButton/TagButton.stories.jsx
+++ src/UI-kit/TagButton/TagButton.stories.jsx
@@ -1,18 +1,31 @@
+import React, {useState} from "react";
 import TagButton from './TagButton';
-import './styles.module.scss';
 
 export default {
   title: 'UI-kit/TagButton',
   component: TagButton,
   // argTypes: {
 
-  // }
+  // },
+  args: {
+    value: 'TagButton'
+  }
 }
 
-export const Basic = {
+/*export const Basic = {
   args: {
     // values: '',
     value: 'TagButton',
     handleChange: () => { },
   }
-}
+}*/
+
+export const Basic = (props) => {
+  const [state, setState] = useState(false);
+
+  return (
+    <div className="storybook-case-wrapper">
+      <TagButton {...props} isEnabled={state} onChange={setState} />
+    </div>
+  )
+};
--- src/components/TagSection/TagSection.stories.jsx
+++ src/components/TagSection/TagSection.stories.jsx
@@ -1,9 +1,7 @@
 import { useState, useEffect } from 'react';
-import { apiEvents } from '../../utils/api';
 import { TagSection } from './TagSection';
 import { TagButton } from '../../UI-kit';
-import styles from './styles.module.scss';
-//import { useFiltersContext } from "../../utils/context/SearchFilterContext";
+import { FiltersProvider } from "../../utils/context/SearchFilterContext";
 
 export default {
   title: 'Components/TagSection',
@@ -11,9 +9,6 @@ export default {
   parameters: {
     layout: 'centered',
     componentSubtitle: 'Components > TagSection',
-  },
-  args: {
-    handleChange: 'function',
   },
   argTypes: {
     handleChange: {
@@ -23,55 +18,19 @@ export default {
 };
 /** Применимость: LeftFilterBar */
 export const Section = () => {
-  const [tags, setTags] = useState([]);
-  const [showAllTags, setShowAllTags] = useState(false);
-  //const { values } = useFiltersContext()
+  const onChange = (value) => (isEnabled) => {};
 
-  useEffect(() => {
-    const fetchTags = async () => {
-      try {
-        const response = await apiEvents.getTags();
-        setTags(response);
-      } catch (error) {
-        console.log('Error fetching tags:', error);
-      }
-    };
-    fetchTags();
-  }, []);
-
-  const toggleShowAllTags = () => {
-    setShowAllTags(!showAllTags);
-  };
-
-  const tagOptions = tags.map((tag) => ({
-    value: tag.id,
-    label: tag.name,
-    slug: tag.slug,
-  }));
-
-  return (
-    <>
-      <div className={styles.tagsList} style={{ maxWidth: '215px' }}>
-        {tagOptions
-          .slice(0, showAllTags ? tags.length : 9)
-          .map((tag, index) => (
-            <TagButton
-              key={index}
-              //values={values}
-              value={tag.label}
-              handleChange={(e) => e.preventDefault}
-            />
-          ))}
-      </div>
-      {tags.length > 10 && (
-        <button onClick={toggleShowAllTags} className={styles.showMore}>
-          {showAllTags ? 'Свернуть все' : 'Показать больше'}
-        </button>
-      )}
-    </>
-  );
+  return <FiltersProvider>
+    <TagSection
+      fetchTags={async () => ([
+        { id: 1, name: "tag 1", slug: "tag1" },
+        { id: 1, name: "tag 2", slug: "tag2" }
+      ])}
+      handleChange={onChange}
+    />
+  </FiltersProvider>
 };
 /** Применимость: TagSection */
 export const Tag = () => {
-  return <TagButton value='Frontend' handleChange={(e) => e.preventDefault} />;
+  return <TagButton value='Frontend' onChange={() => {}} />;
 };
